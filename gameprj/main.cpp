262531849a44dab9d65e30fc6962cacce9f460b0
NDH2611 <hoadztv11@gmail.com>
2025-02-20 21:22:43 +0700

    playermove

---
+++ gameprj/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "graphics.h"
+#include "defs.h"
+#include "player_move.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Graphics graphics;
+    graphics.init();
+
+    Mouse mouse;
+    mouse.x = SCREEN_WIDTH / 2;
+    mouse.y = 580;
+
+    bool quit = false;
+    SDL_Event event;
+    bool moving = false;
+    while (!quit && !gameOver(mouse)) {
+        graphics.prepareScene();
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) quit = true;
+
+            if (event.type == SDL_KEYDOWN)
+            {
+                moving = true;
+                switch (event.key.keysym.scancode)
+                {
+                    case SDL_SCANCODE_W:
+                        mouse.turnNorth();
+                        break;
+                    case SDL_SCANCODE_S:
+                        mouse.turnSouth();
+                        break;
+                    case SDL_SCANCODE_A:
+                        mouse.turnWest();
+                        break;
+                    case SDL_SCANCODE_D:
+                        mouse.turnEast();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (event.type == SDL_KEYUP)
+            {
+                moving = false;
+            }
+        }
+
+        if (moving) {
+            mouse.move();
+        }
+
+        render(mouse, graphics);
+
+        graphics.presentScene();
+        SDL_Delay(10);
+    }
+
+    graphics.quit();
+    return 0;
+}
+
---
+++ gameprj/player_move.h
@@ -0,0 +1,49 @@
+#ifndef PLAYER_MOVE_H
+#define PLAYER_MOVE_H
+
+#define INITIAL_SPEED 5
+
+struct Mouse {
+    int x, y;
+    int dx = 0, dy = 0;
+    int speed = INITIAL_SPEED;
+    void move() {
+        x += dx;
+        y += dy;
+    }
+    void turnNorth() {
+        dy = -speed;
+        dx = 0;
+    }
+    void turnSouth() {
+        dy = speed;
+        dx = 0;
+    }
+    void turnWest() {
+        dy = 0;
+        dx = -speed;
+    }
+    void turnEast() {
+        dy = 0;
+        dx = speed;
+    }
+};
+
+void render(const Mouse& mouse, const Graphics& graphics) {
+    SDL_Rect filled_rect;
+    filled_rect.x = mouse.x;
+    filled_rect.y = mouse.y;
+    filled_rect.w = 20;
+    filled_rect.h = 20;
+    SDL_SetRenderDrawColor(graphics.renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(graphics.renderer, &filled_rect);
+}
+
+bool gameOver(const Mouse& mouse) {
+    return mouse.x < 0 || mouse.x >= SCREEN_WIDTH ||
+           mouse.y < 0 || mouse.y >= SCREEN_HEIGHT;
+}
+
+#endif
+
+
